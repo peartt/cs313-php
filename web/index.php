9f8c557662f016881875ea8340b9fee18725263c
Tyler Peart <tyler.e.peart@gmail.com>
2020-01-18 11:15:38 -0700

    Add index.php and stylesheet.css for homepage

--- web/index.php
+++ web/index.php
@@ -1,5 +1,15 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=devide-width", initial-scale=1.0">
+<title>Tyler Peart Homepage</title>
+<link rel="stylesheet" href="stylesheet.css">
+</head>
 
-  phpinfo();
+<body>
+<h1>Welcome to my homepage!</h1>
+<p>This is some sample text. Lorem ipsulon and stuff</p>
 
-?>
+</body>
+</html>
