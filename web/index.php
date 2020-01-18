683828eb372310b007c82182d2057986facc6f53
Tyler Peart <tyler.e.peart@gmail.com>
2020-01-18 14:14:14 -0700

    Add time feature at bottom-right of webpage

--- web/index.php
+++ web/index.php
@@ -9,9 +9,14 @@
 
 <body>
 <h1>Welcome to my homepage!</h1>
+<!-- Info paragraph "About me" -->
 <p>Hello and welcome to my homepage! My name is Tyler Peart and I am a Computer Science Major. Currently this is my 6th Semester here at Brigham Young University-Idaho and I'm loving
  my time here. I have a lot of interests and hobbies but one thing that I love to do is program. Seeing problems that need to be solved and subsequently writing code to solve the 
  problem is what I love to do. The possiblities of programming are so vast and exciting that I know that this field is the one for me.</p>
 
+<!-- use php to display time at the bottom right of the page -->
+<?php
+echo "<div class='time'> The time is " . date("d/m/Y") . "</div>"; 
+?>
 </body>
 </html>
