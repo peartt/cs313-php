ed5b32c5d4215c8648e19cc85aa88fe8d45acffb
Tyler Peart <tyler.e.peart@gmail.com>
2020-01-18 13:58:52 -0700

    Add an info blurb for homepage, changed stylsheet colors

--- web/index.php
+++ web/index.php
@@ -9,7 +9,9 @@
 
 <body>
 <h1>Welcome to my homepage!</h1>
-<p>This is some sample text. Lorem ipsulon and stuff</p>
+<p>Hello and welcome to my homepage! My name is Tyler Peart and I am a Computer Science Major. Currently this is my 6th Semester here at Brigham Young University-Idaho and I'm loving
+ my time here. I have a lot of interests and hobbies but one thing that I love to do is program. Seeing problems that need to be solved and subsequently writing code to solve the 
+ problem is what I love to do. The possiblities of programming are so vast and exciting that I know that this field is the one for me.</p>
 
 </body>
 </html>
